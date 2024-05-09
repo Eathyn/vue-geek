24fc600e08765b81b9f88bc5e2875841a5df6868
eathyn <eathynzakas@gmail.com>
2024-05-09 19:44:14 +0800

    refactor: ♻️ remove unwanted code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,3 @@
-<script lang="ts" setup>
-let a: string = 'text'
-console.log(a)
-</script>
-
 <template>
   <div>app</div>
 </template>
-
-<style scoped lang="scss">
-div {
-  color: $font-color;
-  text-decoration: dashed;
-}
-</style>
