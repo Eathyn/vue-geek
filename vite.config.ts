66221cd349364e26b03e46350ab2b276d791b293
eathyn <eathynzakas@gmail.com>
2024-05-09 20:37:28 +0800

    perf: ⚡️ only import 'resolve' function

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig, normalizePath } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // 需要安装 @types/node，否则 TS 会报错
-import path from 'path'
+import { resolve } from 'path'
 
 // 需要使用 normalizePath，否则 Vue 会报错找不到该文件
-const variablePath = normalizePath(path.resolve('./src/styles/variables.scss'))
+const variablePath = normalizePath(resolve('./src/styles/variables.scss'))
 
 export default defineConfig({
   resolve: {
