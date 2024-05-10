a417e07f6e58018267a548a2c6c7078fb1ef9d46
eathyn <eathynzakas@gmail.com>
2024-05-10 18:09:48 +0800

    build: 📦️ 安装 `element-plus` 包

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // 需要安装 @types/node，否则 TS 会报错
 import { resolve } from 'path'
+// 按需引入 Element Plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // 需要使用 normalizePath，否则 Vue 会报错找不到该文件
 const variablePath = normalizePath(resolve('./src/styles/variables.scss'))
@@ -13,7 +17,15 @@ export default defineConfig({
       '@': resolve(__dirname, './src'),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   css: {
     preprocessorOptions: {
       scss: {
