553ef03e29835fd96c561c37402b0a5c02bc0e80
eathyn <eathynzakas@gmail.com>
2024-05-11 18:35:47 +0800

    feat: 🚀 配置并引入 NProgress

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import staticRoutes from '@/router/modules/static-route.ts'
 import errorRoutes from '@/router/modules/error-route.ts'
+import NProgress from '@/config/nprogress.ts'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -11,4 +12,6 @@ const router = createRouter({
   }),
 })
 
+// 1.NProgress 开始
+NProgress.start()
 export default router
