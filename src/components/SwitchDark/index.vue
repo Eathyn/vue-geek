14ccb5b1b6103433527ac962dc805466426e6c33
eathyn <eathynzakas@gmail.com>
2024-07-14 16:25:59 +0800

    feat: 🧩 暗黑模式切换组件

---
+++ src/components/SwitchDark/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Sunny, Moon } from '@element-plus/icons-vue'
+
+import useGlobalStore from '@/store/module/global.ts'
+import { useTheme } from '@/hooks/useTheme.ts'
+
+defineOptions({
+  name: 'SwitchDark',
+})
+
+const globalStore = useGlobalStore()
+const themeStore = useTheme()
+</script>
+
+<template>
+  <el-switch
+    v-model="globalStore.isDark"
+    inline-prompt
+    :active-icon="Sunny"
+    :inactive-icon="Moon"
+    @change="themeStore.switchDark"
+  />
+</template>
