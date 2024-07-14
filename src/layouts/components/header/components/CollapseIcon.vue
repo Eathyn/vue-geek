5df979911faa7e34713d51c9171ea2270e16e0b5
eathyn <eathynzakas@gmail.com>
2024-07-14 16:10:20 +0800

    feat: 🧩 伸缩侧边栏按钮组件

---
+++ src/layouts/components/header/components/CollapseIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-icon
+    class="collapse-icon"
+    @click="changeCollapse"
+  >
+    <component :is="globalStore.isCollapse ? Expand : Fold" />
+  </el-icon>
+</template>
+
+<script setup lang="ts">
+import { Expand, Fold } from '@element-plus/icons-vue'
+
+import useGlobalStore from '@/store/module/global.ts'
+
+const globalStore = useGlobalStore()
+
+function changeCollapse() {
+  globalStore.isCollapse = !globalStore.isCollapse
+}
+</script>
+
+<style scoped lang="scss">
+.collapse-icon {
+  margin-right: 20px;
+  font-size: 22px;
+  cursor: pointer;
+}
+</style>
