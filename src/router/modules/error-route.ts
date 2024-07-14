1957c6821e78a870ab3fdd3edf9e66adc6901af4
eathyn <eathynzakas@gmail.com>
2024-07-14 15:17:46 +0800

    feat: 🧩 error views

--- src/router/modules/error-route.ts
+++ src/router/modules/error-route.ts
@@ -4,17 +4,17 @@ const errorRoutes: RouteRecordRaw[] = [
   {
     path: '/403',
     name: '403',
-    component: () => import('@/view/error/403.vue'),
+    component: () => import('@/views/error/403.vue'),
   },
   {
     path: '/404',
     name: '404',
-    component: () => import('@/view/error/404.vue'),
+    component: () => import('@/views/error/404.vue'),
   },
   {
     path: '/500',
     name: '500',
-    component: () => import('@/view/error/500.vue'),
+    component: () => import('@/views/error/500.vue'),
   },
 ]
 
