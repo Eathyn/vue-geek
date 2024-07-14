247ccbba16ea366172bf0db671bb85869b5a6b20
eathyn <eathynzakas@gmail.com>
2024-07-14 16:04:19 +0800

    feat: 🧩 头部

---
+++ src/layouts/components/header/ToolBarLeft.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="tool-bar-lf">
+    <CollapseIcon id="collapseIcon" />
+    <Breadcrumb
+      v-if="globalStore.isBreadcrumb"
+      id="breadcrumb"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CollapseIcon from '@/layouts/components/header/components/CollapseIcon.vue'
+import Breadcrumb from '@/layouts/components/header/components/Breadcrumb.vue'
+import useGlobalStore from '@/store/module/global.ts'
+
+const globalStore = useGlobalStore()
+</script>
+
+<style scoped lang="scss">
+.tool-bar-lf {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  white-space: nowrap;
+}
+</style>
---
+++ src/layouts/components/header/ToolBarRight.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tool-bar-ri">
+    <div class="header-icon">
+      <ThemeSetting />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ThemeSetting from '@/layouts/components/header/components/ThemeSetting.vue'
+
+defineOptions({
+  name: 'ToolBarRight',
+})
+</script>
+
+<style scoped lang="scss"></style>
